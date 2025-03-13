53bfc1ec453db3dfdae0696dbbe0daafa465313e
Bryson Camp <bryson@bryson.camp>
2025-03-13 11:18:57 +1000

    Updated Transcript, reverted layout

--- script.js
+++ script.js
@@ -18,16 +18,6 @@ const projects = [
         title: "DropEntry",
         description: "Automate data entry for Xero",
         tags: ["Web App", "React", "AWS", "Xero API"]
-    },
-    {
-        title: "ProjectName 5",
-        description: "Short description of the project 5.",
-        tags: ["Tag51", "Tag52", "Tag53"]
-    },
-    {
-        title: "ProjectName 6",
-        description: "Short description of the project 6.",
-        tags: ["Tag61", "Tag62", "Tag63"]
     }
 ]
 
@@ -61,6 +51,7 @@ const addNewItem = (column, position) => {
         const item = getNewItem();
         column.insertAdjacentHTML(position, item);
     });
+    
 }
 
 const removeTopItem = (column) => column.removeChild(column.firstChild);
@@ -108,19 +99,55 @@ columns.forEach((column) => {
     addNewItem(column, "afterbegin");                  
 });
 
+const returnCursor = document.getElementById("returnCursor");
+
 const handleMouseOver = (element) => {
     const hoverItems = document.querySelectorAll(".item-hover");
     hoverItems.forEach((hoverItem) => {
         hoverItem.classList.remove("item-hover");
     });
 
+    const hoverButtons = document.querySelectorAll(".button-hover");
+    hoverButtons.forEach((hoverButton) => {
+        hoverButton.classList.remove("button-hover");
+    });
+
     const closestItem = element.closest(".item");
-    if (closestItem) {
+    const mouseOverUnclickedItem = closestItem && !closestItem.classList.contains("item-clicked");
+
+    const closestButton = element.closest(".button");
+    const canReturn = !closestItem && document.querySelector(".item-clicked");
+
+    
+    //console.log("-----_");
+    //console.log(closestItem);
+    //console.log(document.querySelector(".item-clicked"));
+
+    if (mouseOverUnclickedItem) {
         closestItem.classList.add("item-hover");
+        mouseCenter.classList.add("mouse-center-large");
+        returnCursor.classList.add("hide-return");
+    } else if (closestButton) {
+        closestButton.classList.add("button-hover");
+        mouseCenter.classList.add("mouse-center-large");
+        returnCursor.classList.add("hide-return");
+    } else if (canReturn) {
+        returnCursor.classList.remove("hide-return");
+        // set cursor as poitner
+        document.body.style.cursor = "pointer";
+
+        //console.log("can return");
+        //closestButton.classList.add("button-hover");
         mouseCenter.classList.add("mouse-center-large");
     } else {
         mouseCenter.classList.remove("mouse-center-large");
     }
+
+    if (!canReturn) {
+        returnCursor.classList.add("hide-return");
+    }
+
+    
 
 }
 
@@ -168,6 +195,13 @@ window.addEventListener("mousemove", (event) => {
 let lastKnownX = 0;
 let lastKnownY = 0;
 
+const calculateRotationAngle = (element, event) => {
+    const rect = element.getBoundingClientRect();
+    const centerX = rect.x + rect.width/2;
+    const centerY = rect.y + rect.height/2;
+    return Math.atan2(event.clientY - centerY, event.clientX - centerX) + Math.PI;
+}
+
 window.addEventListener("mousemove", (event) => {
     lastKnownX = event.clientX;
     lastKnownY = event.clientY;
@@ -175,16 +209,20 @@ window.addEventListener("mousemove", (event) => {
     let rotationAngle = 0;
 
     const closestItem = event.target.closest(".item");
+    const closestButton = event.target.closest(".button");
+
     if (closestItem) {
-        // get the center of that div.
+        /*
         const rect = closestItem.getBoundingClientRect();
         const centerX = rect.x + rect.width/2;
         const centerY = rect.y + rect.height/2;
-        // get the angle between the mouse and the center of the div.
         rotationAngle = Math.atan2(event.clientY - centerY, event.clientX - centerX) + Math.PI;
-
+        console.log("item", rotationAngle);*/
+        rotationAngle = calculateRotationAngle(closestItem, event);
+    } else if (closestButton) {
+        rotationAngle = calculateRotationAngle(closestButton, event);
     }
-    console.log(closestItem);
+
 
     // want to rotate the arrow to be towards the center of the div it is on. 
     // get the .item class that is closest to the mouse.
@@ -208,6 +246,11 @@ document.addEventListener("click", (event) => {
     const item = event.target.closest(".item");
     if(!item) return;
 
+    const newUrl = "/" + item.querySelector(".title").textContent.toLowerCase().replace(" ", "");
+    window.history.pushState({}, '', newUrl);
+    console.log(item.querySelector(".title").textContent.toLowerCase().replace(/\s+/g, ""));
+    console.log("click", item);
+
     scrollEnabled = false;
 
     main.classList.add("main-single");
@@ -218,8 +261,26 @@ document.addEventListener("click", (event) => {
 
     item.classList.add("item-clicked");
     scrollEnabled = false;
-    console.log("click", item);
 
     item.scrollIntoView({ behavior: 'smooth', block: 'start' });
 
+    // ----------------------
+
+    //item.classList.add("item-hover");
+
 });
+
+
+/*
+
+const aboutButton = document.getElementById("aboutButton");
+const aboutText = document.getElementById("aboutText");
+
+aboutButton.addEventListener("click", () => {
+    aboutText.classList.toggle("hide-height");
+    aboutButton.classList.toggle("button-selected");
+});
+
+
+*/
+
